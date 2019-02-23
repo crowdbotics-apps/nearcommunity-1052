ae66dc5078964900032e78efdc6193a41b45be14
Crowdbotics <team@crowdbotics.com>
2019-02-23 06:49:38 +0000

    initial commit

---
+++ android/app/src/main/java/com/nearcommunity_1052/MainActivity.java
@@ -0,0 +1,15 @@
+package com.nearcommunity_1052;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "nearcommunity_1052";
+    }
+}
